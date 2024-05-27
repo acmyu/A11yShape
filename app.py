fa5d9a56483a98344aa9350f6ae77a8169f29f7a
acmyu <amycmyu@gmail.com>
2024-05-27 17:03:34 -0400

    simple flask

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, Response, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return 'Hello from Flask1!'
+
+@app.route('/code2fab', methods=['POST'])
+def code2fab():
+    req = request.json
+    #return jsonify(req['code'])
+    return request.json
