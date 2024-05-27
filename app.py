7d338e931bdba6653221fbf8af113cf353036294
acmyu <amycmyu@gmail.com>
2024-05-27 17:39:55 -0400

    fix cors

--- app.py
+++ app.py
@@ -4,6 +4,14 @@ app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response
+
+
 @app.route('/')
 @cross_origin(origin='*')
 def hello_world():
