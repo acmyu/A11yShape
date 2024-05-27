5e0bf6b0a70613d0a5f5cfda4b485a3a89d5d57e
acmyu <amycmyu@gmail.com>
2024-05-27 17:21:00 -0400

    fix cors

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask, Response, request, jsonify
-
+from flask_cors import CORS, cross_origin
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
+@cross_origin()
 def hello_world():
     return 'Hello from Flask1!'
 
 @app.route('/code2fab', methods=['POST'])
+@cross_origin()
 def code2fab():
     req = request.json
     #return jsonify(req['code'])
