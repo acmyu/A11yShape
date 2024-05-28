5f4f871fe316060c6f3e861e409083a7c25fc93c
acmyu <amycmyu@gmail.com>
2024-05-28 00:52:16 -0400

    add index page

--- app.py
+++ app.py
@@ -1,25 +1,18 @@
 from flask import Flask, Response, request, jsonify
 from flask_cors import CORS, cross_origin
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
-
-@app.after_request
-def after_request(response):
-  response.headers.add('Access-Control-Allow-Origin', '*')
-  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-  return response
 
 
 @app.route('/')
-@cross_origin(origin='*')
 def hello_world():
-    return 'Hello from Flask1!'
+    return 'Hello from Flask6!'
 
 @app.route('/code2fab', methods=['POST'])
-@cross_origin(origin='*')
 def code2fab():
     req = request.json
     #return jsonify(req['code'])
-    return request.json
+    return jsonify(request.json)
+
+
